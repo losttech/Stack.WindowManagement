0c7cedf4287eae6b3476f5f6acc83d5258c131ec
Victor Milovanov <lost@losttech.software>
2018-08-09 18:16:42 -0700

    enabled automatic versioning

---
+++ AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyCompany("Lost Tech LLC")]
+[assembly: AssemblyProduct("Stack")]
+[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2018")]
+
+[assembly: ComVisible(false)]
