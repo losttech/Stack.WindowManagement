6fe6d0b5186e092c61db2bca3661f7dddb8b745a
Victor Milovanov <lost@losttech.software>
2019-10-09 22:22:25 -0700

    updated copyright year

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.1.*")]
 [assembly: AssemblyCompany("Lost Tech LLC")]
 [assembly: AssemblyProduct("Stack")]
-[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2018")]
+[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2019")]
 
 [assembly: ComVisible(false)]
