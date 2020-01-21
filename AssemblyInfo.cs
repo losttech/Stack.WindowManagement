5a4f71fcbe33c7aebd5e70d9a1be5cc7f6b39aae
Victor Milovanov <lost@losttech.software>
2020-01-21 13:07:58 -0800

    removed dependency on App Boilerplate

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.2.*")]
 [assembly: AssemblyCompany("Lost Tech LLC")]
 [assembly: AssemblyProduct("Stack")]
 [assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2019")]
